3e1bc99445fa610225af35dd1d7a928fd0b95ad4
김도윤 <kdy0096@gmail.com>
2025-02-18 22:40:46 +0900

    step1

---
+++ ShinyDay/ShinyDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShinyDay
+//
+//  Created by yoonie on 2/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
