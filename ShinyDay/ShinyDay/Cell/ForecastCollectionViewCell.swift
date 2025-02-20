f76a5d49a1bfdf87fd8bda099445a7644c30a9e1
김도윤 <kdy0096@gmail.com>
2025-02-21 01:31:30 +0900

    IBOutlet 설정

---
+++ ShinyDay/ShinyDay/Cell/ForecastCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  ForecastCollectionViewCell.swift
+//  ShinyDay
+//
+//  Created by yoonie on 2/21/25.
+//
+
+import UIKit
+
+class ForecastCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+}
---
+++ ShinyDay/ShinyDay/Cell/SummaryCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  SummaryCollectionViewCell.swift
+//  ShinyDay
+//
+//  Created by yoonie on 2/21/25.
+//
+
+import UIKit
+
+class SummaryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var minMaxLabel: UILabel!
+    
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    
+}
